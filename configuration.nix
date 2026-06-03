22a1d536255bef50441d808bbb463ad6f93750dc
rej0y <zhoushengjian1@gmail.com>
2026-06-03 17:01:55 -0600

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,83 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout = 0;
+  };
+
+  networking = {
+    hostName = "altruist";
+    networkmanager.enable = true;
+  };
+
+  users.users.rej0y = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpkmA0Ako1CSQTj2grWHPC55etVCaepAIs+qv9ljbAF rej0y@charity"
+    ];
+  };
+
+  security.rtkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  services = {
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
+
+    frp.instances = {
+      ssh = {
+        enable = true;
+        role = "client";
+
+        settings = {
+          serverAddr = "66.112.209.106";
+          serverPort = 7000;
+        
+          proxies = [
+            {
+              name = "ssh";
+              type = "tcp";
+              localIP = "127.0.0.1";
+              localPort = 22;
+              remotePort = 2222;
+            }
+          ];
+        };
+      };
+    };
+  };
+
+  time.timeZone = "America/Boise";
+  
+  system.stateVersion = "26.05";
+}
+
---
+++ home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "rej0y";
+    homeDirectory = "/home/rej0y";
+    stateVersion = "26.05";
+
+    packages = with pkgs; [
+      kitty
+    ];
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      initLua = ''
+        vim.opt.expandtab = true
+        vim.opt.tabstop = 2
+        vim.opt.shiftwidth = 2
+        vim.opt.softtabstop = 2
+      '';
+    };
+
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+
+    bash = {
+      enable = true;
+    };
+
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "rej0y";
+          email = "zhoushengjian1@gmail.com";
+        };
+        
+        url = {
+          "git@github.com:".insteadOf = [ "https://github.com/" ];
+        };
+
+        init.defaultBranch = "main";
+      };
+    };
+  };
+}
