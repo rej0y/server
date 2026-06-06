28c73caaa7c510be3d6c8317858a9c50874ece7f
rej0y <zhoushengjian1@gmail.com>
2026-06-06 15:30:49 -0600

    add server package

--- modules/atm10.nix
+++ modules/atm10.nix
@@ -4,7 +4,7 @@ let
   atm10Version = "7.0";
   atm10ServerZip = pkgs.fetchurl {
     url = "https://mediafilez.forgecdn.net/files/8094/893/ServerFiles-7.0.zip";
-    hash = lib.fakeHash;
+    hash = "sha256-b3xzqChHx5YcrF2cV5svL096K2RtqA84soyKQG4nn2A=";
   };
 in
 {
