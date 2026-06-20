8681c62f6806846904472e04372b49fe4aaf8c8e
rej0y <zhoushengjian1@gmail.com>
2026-06-20 05:55:21 -0600

    commit

--- modules/spring-break/default.nix
+++ modules/spring-break/default.nix
@@ -7,34 +7,36 @@ let
     sha256 = "0555a0b0468a5198d8fb1a16e1f9e95c81a917a2dc8f2e09867b4044742f6401";
   };
 
-  java = "${pkgs.jdk25}/bin/java";
-  memoryArgs = [ "-Xms2G" "-Xmx16G" ];
-  jvmArgs = [
-    "-XX:+UseZGC"
-    "-XX:+DisableExplicitGC"
-  ];
-
-  serverDir = "/var/lib/spring-break";
-  serverJar = pkgs.fetchurl {
-    name = "paper-${paper.version}-${paper.build}.jar";
-    url = "https://fill-data.papermc.io/v1/objects/${paper.sha256}/paper-${paper.version}-${paper.build}.jar";
-    curlOptsList = [ "-A" "spring-break-server/1.0 (https://github.com/rej0y/server)" ];
-    hash = builtins.convertHash {
-      hash = paper.sha256;
-      hashAlgo = "sha256";
-      toHashFormat = "sri";
+  java = {
+    exec = "${pkgs.jdk25}/bin/java";
+    args = [
+      "-Xms2G"
+      "-Xmx16G"
+      "-XX:+UseZGC"
+      "-XX:+DisableExplicitGC"
+    ];
+  };
+  
+  server = {
+    dir = "/var/lib/spring-break";
+    jar = pkgs.fetchurl {
+      name = "paper-${paper.version}-${paper.build}.jar";
+      url = "https://fill-data.papermc.io/v1/objects/${paper.sha256}/paper-${paper.version}-${paper.build}.jar";
+      curlOptsList = [ "-A" "spring-break-server/1.0 (https://github.com/rej0y/server)" ];
+      hash = builtins.convertHash {
+        hash = paper.sha256;
+        hashAlgo = "sha256";
+        toHashFormat = "sri";
+      };
     };
   };
-
-  plugins = import ./plugins.nix { inherit pkgs; };
-  linkPlugins = map (plugin: "${pkgs.coreutils}/bin/ln -sfn ${plugin.jar} ${serverDir}/plugins/${plugin.fileName}") plugins;
 in
 {
   users.groups.spring-break = {};
   users.users.spring-break = {
     isSystemUser = true;
     group = "spring-break";
-    home = serverDir;
+    home = server.dir;
   };
 
   systemd.services.spring-break = {
@@ -45,11 +47,10 @@ in
       User = "spring-break";
       Group = "spring-break";
       StateDirectory = "spring-break";
-      WorkingDirectory = serverDir;
+      WorkingDirectory = server.dir;
       Restart = "on-failure";
       RestartSec = "15s";
-      ExecStartPre = [ "${pkgs.coreutils}/bin/mkdir -p ${serverDir}/plugins" ] ++ linkPlugins;
-      ExecStart = "${java} ${lib.escapeShellArgs (memoryArgs ++ jvmArgs ++ [ "-jar" serverJar "nogui" ])}";
+      ExecStart = "${java.exec} ${lib.escapeShellArgs (java.args ++ [ "-jar" server.jar "nogui" ])}";
       SuccessExitStatus = "0 143";
     };
   };
