fec0d3f05480665eb25a198f198543591835246f
rej0y <zhoushengjian1@gmail.com>
2026-07-08 20:24:51 -0600

    Add more whitespace

--- modules/spring-break/default.nix
+++ modules/spring-break/default.nix
@@ -3,8 +3,10 @@
 let
   settings = import ./settings.nix { inherit pkgs; };
   plugins = import ./plugins.nix { inherit pkgs; };
+
   server = {
     dir = "/var/lib/spring-break";
+
     jar = pkgs.fetchurl {
       name = "paper-${settings.paper.version}-${settings.paper.build}.jar";
       url = "https://fill-data.papermc.io/v1/objects/${settings.paper.sha256}/paper-${settings.paper.version}-${settings.paper.build}.jar";
@@ -15,6 +17,7 @@ let
         toHashFormat = "sri";
       };
     };
+
     init = pkgs.writeShellScript "server-init" ''
       set -eu
       ${pkgs.coreutils}/bin/install -Dm0640 ${
